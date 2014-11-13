055fe37fd2b08e8661b82dd9fe14dfa76962f114
Alejandro G-G Pérez <alex.ggperez@gmail.com>
2014-11-13 14:21:29 +0100

    Añadidos lista de bares e info de bar, underscore.js

--- MisBares/static/js/bares.js
+++ MisBares/static/js/bares.js
@@ -1,18 +1,49 @@
 $(document).ready(function(){
 
+    
+
+
+
     map = L.map('map').setView([40.4405295, -3.6927629], 15);
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);  
        
     
-    (function paintBars(){
+    //bar_list is the list of bars given by django, sorted by price
+    bar_list = _.sortBy(bar_list, function(obj){ return obj.fields.price;});    
+    
+    (function paintBars(){          //this function creates markers on the map
         for (i in bar_list){
-            var marker = L.marker([bar_list[i].fields.latitude, bar_list[i].fields.longitude]).addTo(map);    
-            marker.bindPopup(bar_list[i].fields.name+'<br>'+bar_list[i].fields.price+'€').openPopup();        
+            var marker = new L.marker([bar_list[i].fields.latitude, bar_list[i].fields.longitude],{
+                        title:bar_list[i].fields.name+' '+bar_list[i].fields.price+'€'
+                        }).addTo(map);    
+            marker.on('click',function(e) {
+                    var latlng=this.getLatLng();
+                    var barfound = _.find(bar_list, function(obj){ 
+                                        return obj.fields.latitude == latlng.lat && obj.fields.longitude == latlng.lng; });
+                    fillBarInfo(barfound);
+            });
         }
     })();
-        
+    
+   (function fillBarList(){         //this function list the bars on the left side of the map inside the div "barList"
+        for (i in bar_list){
+            $('#barList ul').append('<li id='+bar_list[i].pk+'>'+bar_list[i].fields.name+' <span>'+bar_list[i].fields.price+'€'+'</li>');
+        }
+   
+   })();
+   
+   $( '#barList li' ).click(function( event ) {
+      var pk=this.id;
+      var barfound = _.find(bar_list, function(obj){ 
+                                        return obj.pk == pk; });
+      fillBarInfo(barfound);
+    });
+   
+   function fillBarInfo(bar){       //this function print the info of the objet bar given as a parameter
+        $('#barInfo').html('<h3>'+bar.fields.name+'</h3> Precio de la caña: '+bar.fields.price+'€')
+   };
     
 });
 
